a7df228d8e6654b7e01085709dca59f38f6f3465
Davide <dav.valiante@gmail.com>
2018-07-18 11:58:52 +0200

    changed listen log

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const PORT = process.env.PORT || 5000;
 
-const html = path.join(__dirname,'/index.html');
 
 app.get('/', (req, res) => {
     res.send({'some_key':'some_value'})
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
