4d11ff54d90b254ff8745df3463bff33b6c3ff63
Davide <dav.valiante@gmail.com>
2018-07-18 11:00:28 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const html = path.join(__dirname,'/index.html');
+
+app.get('/', (req, res) => {
+    res.send({'some_key':'some_value'})
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(HEROKU_PORT);
